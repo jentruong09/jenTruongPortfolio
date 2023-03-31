6dfdad660ec49fe2c680ae3668ba86ac2331d792
Jen Truong <jennifertruong09@gmail.com>
2023-03-31 12:21:08 -0400

    added some media queries and js for smaller px screens as well as navbar funtions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// toggle icon nav
+let menuIcon = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    nav.classList.toggle('active')
+}
+
+
 // scroll function
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
@@ -13,7 +23,17 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        }
-    })
-}
+            });
+        };
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+
+    // remove toggle icon and nav after clicking nav link
+    menuIcon.classList.remove('bx-x')
+    nav.classList.remove('active')
+};
